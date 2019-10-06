a23b4565a165bd8c601d4d1790ba61e92de2c8a5
Zackere <wo1001@wp.pl>
2019-10-06 16:38:08 +0200

    Add controller interface

---
+++ src/controllers/controller.hpp
@@ -0,0 +1,22 @@
+// Copyright Wojciech Replin 2019
+
+#pragma once
+
+#include <Windows.h>
+
+namespace gk {
+class DrawingBoard;
+
+class Controller {
+ public:
+  virtual bool OnMouseLButtonDown(DrawingBoard* board, POINT mouse_pos) = 0;
+  virtual bool OnMouseLButtonUp(DrawingBoard* board, POINT mouse_pos) = 0;
+  virtual bool OnMouseLButtonDoubleClick(DrawingBoard* board,
+                                         POINT mouse_pos) = 0;
+  virtual bool OnMouseMove(DrawingBoard* board, POINT mouse_pos) = 0;
+  virtual bool OnKeyDown(DrawingBoard* board,
+                         WPARAM key_code,
+                         bool was_down) = 0;
+  virtual bool OnKeyUp(DrawingBoard* board, WPARAM key_code) = 0;
+};
+}  // namespace gk
